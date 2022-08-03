ef74c6aa0c6fb252a0dac9c2c91b3e91bae6abbc
Charlie Egan <charlieegan3@users.noreply.github.com>
2022-08-03 23:02:12 +0100

    Implement getting values

--- internal/pkg/exif/exif.go
+++ internal/pkg/exif/exif.go
@@ -3,12 +3,68 @@ package exif
 import (
 	"fmt"
 	"io/ioutil"
+	"strconv"
 	"strings"
 	"time"
 
 	"github.com/dsoprea/go-exif/v3"
 	exifcommon "github.com/dsoprea/go-exif/v3/common"
 )
+
+func SetKeyString(image, key, value string) error {
+	return nil
+}
+
+func GetKeyString(image, key string) (string, error) {
+	b, err := ioutil.ReadFile(image)
+	if err != nil {
+		return "", fmt.Errorf("failed to read image file: %w", err)
+	}
+
+	rawExif, err := exif.SearchAndExtractExif(b)
+	if err == exif.ErrNoExif {
+		return "", fmt.Errorf("no exif data found")
+	} else if err != nil {
+		return "", fmt.Errorf("failed to get raw exif data: %s", err)
+	}
+
+	im, err := exifcommon.NewIfdMappingWithStandard()
+	if err != nil {
+		return "", fmt.Errorf("failed to create idfmapping: %s", err)
+	}
+
+	ti := exif.NewTagIndex()
+
+	_, index, err := exif.Collect(im, ti, rawExif)
+	if err != nil {
+		return "", fmt.Errorf("failed to collect exif data: %s", err)
+	}
+
+	var value string
+	cb := func(ifd *exif.Ifd, ite *exif.IfdTagEntry) error {
+		if ite.TagName() == key {
+			rawValue, err := ite.Value()
+			if err != nil {
+				return fmt.Errorf("could not get raw value")
+			}
+
+			var ok bool
+			value, ok = rawValue.(string)
+			if !ok {
+				return fmt.Errorf("value was not in expected format: %#v", rawValue)
+			}
+		}
+
+		return nil
+	}
+
+	err = index.RootIfd.EnumerateTagsRecursively(cb)
+	if err != nil {
+		return "", fmt.Errorf("failed to walk exif data tree: %s", err)
+	}
+
+	return value, nil
+}
 
 func GetUTC(image string) (time.Time, error) {
 	b, err := ioutil.ReadFile(image)
@@ -56,18 +112,22 @@ func GetUTC(image string) (time.Time, error) {
 				return fmt.Errorf("failed to parse DateTimeOriginal: %s", err)
 			}
 		}
-		if ite.TagName() == "SubsecTimeOriginal" {
+		if ite.TagName() == "SubSecTimeOriginal" {
 			rawValue, err := ite.Value()
 			if err != nil {
-				return fmt.Errorf("could not get raw SubsecTimeOriginal value")
+				return fmt.Errorf("could not get raw SubSecTimeOriginal value")
 			}
 
-			val, ok := rawValue.(int)
+			val, ok := rawValue.(string)
 			if !ok {
-				return fmt.Errorf("SubsecTimeOriginal was not in expected format: %#v", rawValue)
+				return fmt.Errorf("SubSecTimeOriginal was not in expected format: %#v", rawValue)
 			}
 
-			dateTimeOriginalSubSec = val
+			intValue, err := strconv.ParseInt(val, 10, 64)
+			if err != nil {
+				return fmt.Errorf("failed to parse SubSecTimeOriginal value: %w", err)
+			}
+			dateTimeOriginalSubSec = int(intValue)
 		}
 
 		if ite.TagName() == "OffsetTimeOriginal" {
--- internal/pkg/exif/exif_test.go
+++ internal/pkg/exif/exif_test.go
@@ -3,9 +3,83 @@ package exif
 import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"io"
+	"io/ioutil"
+	"os"
 	"testing"
 	"time"
 )
+
+func TestSetKeyString(t *testing.T) {
+	testCases := map[string]struct {
+		Image string
+		Key   string
+		Value string
+	}{
+		"set DateTimeOriginal": {
+			Image: "./fixtures/iphone.JPG",
+			Key:   "DateTimeOriginal",
+			Value: "2022:08:03 17:56:22",
+		},
+	}
+
+	for name, testCase := range testCases {
+		t.Run(name, func(t *testing.T) {
+			imageCopy, err := ioutil.TempFile(".", "imagee_")
+			require.NoError(t, err)
+			defer os.Remove(imageCopy.Name())
+
+			imageFile, err := os.Open(testCase.Image)
+			require.NoError(t, err)
+
+			_, err = io.Copy(imageCopy, imageFile)
+			require.NoError(t, err)
+
+			err = SetKeyString(imageCopy.Name(), testCase.Key, testCase.Value)
+			require.NoError(t, err)
+
+			readValue, err := GetKeyString(imageCopy.Name(), testCase.Key)
+			require.NoError(t, err)
+
+			assert.Equal(t, testCase.Value, readValue)
+		})
+		continue
+	}
+}
+
+func TestGetKeyString(t *testing.T) {
+	testCases := map[string]struct {
+		Image         string
+		Key           string
+		ExpectedValue string
+	}{
+		"get DateTimeOriginal": {
+			Image:         "./fixtures/iphone.JPG",
+			Key:           "DateTimeOriginal",
+			ExpectedValue: "2022:08:03 18:56:22",
+		},
+		"get OffsetTimeOriginal": {
+			Image:         "./fixtures/iphone.JPG",
+			Key:           "OffsetTimeOriginal",
+			ExpectedValue: "+01:00",
+		},
+		"get SubsecTimeOriginal": {
+			Image:         "./fixtures/iphone.JPG",
+			Key:           "SubSecTimeOriginal",
+			ExpectedValue: "480",
+		},
+	}
+
+	for name, testCase := range testCases {
+		t.Run(name, func(t *testing.T) {
+			value, err := GetKeyString(testCase.Image, testCase.Key)
+			require.NoError(t, err)
+
+			assert.Equal(t, testCase.ExpectedValue, value)
+		})
+		continue
+	}
+}
 
 func TestGetUTC(t *testing.T) {
 	testCases := map[string]struct {
@@ -14,23 +88,23 @@ func TestGetUTC(t *testing.T) {
 	}{
 		"when iphone": {
 			Image:           "./fixtures/iphone.JPG",
-			ExpectedUTCTime: time.Date(2022, time.August, 3, 17, 56, 22, 0, time.UTC),
+			ExpectedUTCTime: time.Date(2022, time.August, 3, 17, 56, 22, 480, time.UTC),
 		},
 		"when iphone in another timezone": {
 			Image:           "./fixtures/iphone_other_tz.JPG",
-			ExpectedUTCTime: time.Date(2022, time.July, 30, 17, 57, 04, 0, time.UTC),
+			ExpectedUTCTime: time.Date(2022, time.July, 30, 17, 57, 04, 349, time.UTC),
 		},
 		"when iphone with no offset": {
 			Image:           "./fixtures/iphone_no_offset.JPG",
-			ExpectedUTCTime: time.Date(2022, time.January, 21, 9, 9, 0, 0, time.UTC),
+			ExpectedUTCTime: time.Date(2022, time.January, 21, 9, 9, 0, 97, time.UTC),
 		},
 		"when iphone moment DNG": {
 			Image:           "./fixtures/moment.DNG",
-			ExpectedUTCTime: time.Date(2022, time.August, 3, 17, 55, 54, 0, time.UTC),
+			ExpectedUTCTime: time.Date(2022, time.August, 3, 17, 55, 54, 222, time.UTC),
 		},
 		"when iphone HIEC": {
 			Image:           "./fixtures/iphone.HEIC",
-			ExpectedUTCTime: time.Date(2022, time.August, 3, 17, 57, 45, 0, time.UTC),
+			ExpectedUTCTime: time.Date(2022, time.August, 3, 17, 57, 45, 986, time.UTC),
 		},
 		"when x100f jpg with no offset": {
 			Image:           "./fixtures/x100f.jpg",
